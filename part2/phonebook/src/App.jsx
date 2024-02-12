db0e166e9e7e57ec82652b2dc6df439e8fac7641
Nevin Leh <nevinleh@proton.me>
2024-02-12 12:43:19 -0700

    Done through exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from "axios"
 import Filter from "./components/Filter"
 import PersonForm from './components/PersonForm'
 import SearchedPersons from './components/SearchedPersons'
 
-const App = () => {
-  const init = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
 
-  const [persons, setPersons] = useState(init)
+const App = () => {
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [search, setSearch] = useState("")
+
+  useEffect(
+    //first argument
+    () => {
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          setPersons(response.data)
+        })
+    },
+    //second argument. Tells use effect only run on first render
+    []
+  )
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
