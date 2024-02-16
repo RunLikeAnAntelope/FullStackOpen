003960e846c14762d3022160b409bea5f99027c9
Nevin Leh <nevinleh@proton.me>
2024-02-15 21:49:42 -0700

    mostly done with countries

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,29 +1,62 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const api_key = import.meta.env.VITE_SOME_KEY
+
 const ShowCountryDetails = ({ countryName }) => {
   const [details, setDetails] = useState(undefined)
+  const [weather, setWeather] = useState(undefined)
 
   useEffect(() => {
-  axios
-    .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countryName}`)
-    .then(response => setDetails(response.data))
+    if (details) {
+      console.log("what is going on");
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${details.capital}&appid=${api_key}`)
+        .then(response => setWeather(response.data))
+    }
+  }, [countryName, details])
+
+  useEffect(() => {
+    console.log("api");
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countryName}`)
+      .then(response => setDetails(response.data))
   }, [countryName])
 
-  if (details) {
+  if (details && weather) {
     return (
       <>
         <h1>{details.name.common}</h1>
         <p>capital {details.capital}</p>
         <p>area {details.area}</p>
-        <h2>languages:</h2>
-        {Object.values(details.languages).map(l => <p key={l}>{l}</p>)}
-        <img src={details.flags.png}/>
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(details.languages).map(l => <li key={l}>{l}</li>)}
+        </ul>
+        <img src={details.flags.png} />
+        <h2>Weather in {details.capital}</h2>
+        <p>temperature {(weather.main.temp - 273).toFixed(2)} Celcius</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+        <p>wind {weather.wind.speed}m/s</p>
       </>
     )
   } else {
     return undefined
   }
+}
+
+const ShowCountry = ({ countryName }) => {
+  const [show, setShow] = useState(false)
+  const onClickHandler = () => setShow(!show)
+  return (
+    <>
+      <p>
+        {countryName}
+        <button onClick={onClickHandler}>show</button>
+      </p>
+      {show ? <ShowCountryDetails countryName={countryName} /> : undefined}
+    </>
+  )
 }
 
 const ShowCountries = ({ countries }) => {
@@ -38,7 +71,7 @@ const ShowCountries = ({ countries }) => {
   } else if (countries.length < 11) {
     return (
       <>
-        {countries.map(c => <p key={c}>{c}</p>)}
+        {countries.map(c => <ShowCountry key={c} countryName={c} />)}
       </>
     )
   } else {
@@ -47,6 +80,7 @@ const ShowCountries = ({ countries }) => {
     )
   }
 }
+
 function App() {
   const [countries, setCountries] = useState(undefined)
   const [searchText, setSearchText] = useState("")
@@ -58,9 +92,7 @@ function App() {
       .then(response => {
         setCountries(response.data.map(c => c.name.common))
       })
-  }
-    , []
-  )
+  }, [])
 
   useEffect(() => {
     if (countries) {
